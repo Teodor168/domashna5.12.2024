557bc61992e011ca91cc975f7be1c878206eaa24
Teodor168 <teodor16iliev@gmail.com>
2024-12-05 21:41:46 +0200

    first commit

---
+++ Palindrom/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Palindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give a string: ");
+            string word = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                stack.Push(word[i]);
+            }
+
+            bool isPalindrome = true;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != stack.Pop())
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (isPalindrome==true)
+            {
+                Console.WriteLine("string is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("string is not palindrome");
+            }
+        }
+    }
+}
---
+++ ReverseIzrechenie/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ReverseIzrechenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give a sentence: ");
+            string[] sentence = Console.ReadLine().Split(" ").ToArray();
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                stack.Push(sentence[i]);
+            }
+            Console.WriteLine($"count of words is {stack.Count}");
+            Console.WriteLine(string.Join(" ", stack));
+
+
+
+        }
+    }
+}
